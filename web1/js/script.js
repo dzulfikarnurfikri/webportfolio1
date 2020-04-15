16c8f1f20fdf9c6ad140aac6d2360408de21d275
Dzulfikar Nurfikri <dzulfikarbolang@gmail.com>
2020-04-15 21:30:45 +0700

    Add files via upload

---
+++ web1/js/script.js
@@ -0,0 +1,25 @@
+const txtElement = ['Freelancer', 'Web Developer', 'Graphic Designer'];
+let count = 0;
+let txtIndex = 0;
+let currentTxt = '';
+let words = '';
+
+(function ngetik(){
+
+	if(count == txtElement.length){
+		count = 0;
+	}
+
+	currentTxt = txtElement[count];
+
+	words = currentTxt.slice(0, ++txtIndex);
+	document.querySelector('.efek-ngetik').textContent = words;
+
+	if(words.length == currentTxt.length){
+		count++;
+		txtIndex = 0;
+	}
+
+	setTimeout(ngetik, 500);
+
+})();
